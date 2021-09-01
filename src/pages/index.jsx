c7453afc18963ff158493d5ec6f5d8af972e7709
coffmanjrp <coffmanjrp@gmail.com>
2021-08-31 20:18:37 -0700

    Create layout from design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,54 +14,35 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Next Posts App</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        <ul className={styles.posts}>
+          <li>
+            <p className={styles.postsContent}>
+              I’m working in Figma trying to design a new website that shows all
+              of my tweet
             </p>
-          </a>
-        </div>
-      </main>
+            <p className={styles.postsDate}>8/31/2021</p>
+          </li>
+          <li>
+            <p className={styles.postsContent}>
+              I’m working in Figma trying to design a new website that shows all
+              of my tweet
+            </p>
+            <p className={styles.postsDate}>8/31/2021</p>
+          </li>
+          <li>
+            <p className={styles.postsContent}>
+              I’m working in Figma trying to design a new website that shows all
+              of my tweet
+            </p>
+            <p className={styles.postsDate}>8/31/2021</p>
+          </li>
+        </ul>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <form>
+          <textarea className={styles.formContent}></textarea>
+          <button className={styles.formBtn}>Add New Post</button>
+        </form>
+      </main>
     </div>
   );
 }
