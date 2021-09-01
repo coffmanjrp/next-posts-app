25309d11c1c4f629110931d2c77daf284b074e69
coffmanjrp <coffmanjrp@gmail.com>
2021-09-01 01:35:07 -0700

    Add icons and meta data to posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,20 @@
+import { FaHeart, FaShareAlt } from 'react-icons/fa';
 import styles from './Post.module.scss';
 
 const Post = ({ content, date }) => {
   return (
     <>
       <p className={styles.postsContent}>{content}</p>
-      <p className={styles.postsDate}>{date}</p>
+      <ul className={styles.postsMeta}>
+        <li className={styles.postsMetaData}>
+          <FaHeart /> 34
+        </li>
+        <li className={styles.postsMetaData}>
+          <FaShareAlt />
+          Share
+        </li>
+        <li className={styles.postsMetaData}>{date}</li>
+      </ul>
     </>
   );
 };
